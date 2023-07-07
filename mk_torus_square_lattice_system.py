119e0816a0e8dd8d1faf4ee00eefa9e2314f3fb7
shutakikuchi <81815783+shutakikuchi@users.noreply.github.com>
2023-07-08 00:21:09 +0900

    Add files via upload

---
+++ mk_torus_square_lattice_system.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import random
+import numpy as np
+
+
+# In[4]:
+
+
+#Generate a torus ferromagnetic square L x L lattice system
+def mk_torus_ferromagnetic(L):
+    ising_quad = {}
+    ising_linear = {}
+    k = 0
+    
+    for i in range(L ** 2):
+        if i == L ** 2 - 1:
+            ising_quad["s{0}".format(k), "s{0}".format(k - (L - 1))] = 7
+            ising_quad["s{0}".format(k), "s{0}".format(k - L * (L - 1))] = 7
+        elif i % L == L - 1:
+            ising_quad["s{0}".format(k), "s{0}".format(k - (L - 1))] = 7
+            ising_quad["s{0}".format(k), "s{0}".format(k + L)] = 7
+        elif i >= L * (L - 1):
+            ising_quad["s{0}".format(k), "s{0}".format(k + 1)] = 7
+            ising_quad["s{0}".format(k), "s{0}".format(k - L * (L - 1))] = 7
+        else:
+            ising_quad["s{0}".format(k), "s{0}".format(k + 1)] = 7
+            ising_quad["s{0}".format(k), "s{0}".format(k + L)] = 7
+        k += 1
+
+        ising_linear['s{0}'.format(i)] = 7  
+        
+    return ising_quad, ising_linear
+
+
+# In[6]:
+
+
+#Generate a torus random square L x L lattice system
+def mk_torus_random(L):
+    ising_quad = {}
+    ising_linear = {}
+    k = 0
+    
+    for i in range(L ** 2):
+        #The coefficients of the interactions take integer values from [-7, 7], although 0 was excluded.
+        while True:
+            J1 = random.randint(-7, 7)
+            J2 = random.randint(-7, 7) 
+            #
+            if J1 != 0 and J2 !=0:
+                break
+        if i == L ** 2 - 1:
+            ising_quad["s{0}".format(k), "s{0}".format(k - (L - 1))] = J1
+            ising_quad["s{0}".format(k), "s{0}".format(k - L * (L - 1))] = J2
+        elif i % L == L - 1:
+            ising_quad["s{0}".format(k), "s{0}".format(k - (L - 1))] = J1
+            ising_quad["s{0}".format(k), "s{0}".format(k + L)] = J2
+        elif i >= L * (L - 1):
+            ising_quad["s{0}".format(k), "s{0}".format(k + 1)] = J1
+            ising_quad["s{0}".format(k), "s{0}".format(k - L * (L - 1))] = J2
+        else:
+            ising_quad["s{0}".format(k), "s{0}".format(k + 1)] = J1
+            ising_quad["s{0}".format(k), "s{0}".format(k + L)] = J2
+        k += 1
+        
+        #The coefficients of the magnetic fields take integer values from [-7, 7]
+        h = random.randint(-7, 7)
+        ising_linear['s{0}'.format(i)] = h           
+    
+    return ising_quad, ising_linear
+
